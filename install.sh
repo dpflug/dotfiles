4d30c8a4e06403548ede2aae1eb4c728bbe20c1a
David Pflug <david@tpflug.com>
2010-07-22 08:40:06 -0400

    install.sh: Corrections

--- install.sh
+++ install.sh
@@ -14,20 +14,20 @@ for file in * ; do
         echo "~/.ssh/${file} exists"
     else
         echo "Linking ~/Public/dotfiles/ssh/${file} to ~/.ssh/"
-        ln -s ~/Public/dotfiles/${file} ~/.ssh/
+        ln -s ~/Public/dotfiles/ssh/${file} ~/.ssh/
     fi
 done
 
 
-for file in ~/Public/dotfiles/ ; do
+for file in ~/Public/dotfiles/* ; do
     #Just omitting the directories for now. I'd like to pull them into this loop, too, though.
     #mkdir and ln files, or ln directories? How can I differentiate between dirs to link and dirs that will have local subdirs (ie .config)?
-    if [[ -f $file ]] ; then
-        if [[ -a ~/.${file} ]] ; then
-            echo "~/.${file} exists"
+    if [[ -f $file && ! -x $file ]] ; then
+        if [[ -a ~/.$(basename ${file}) ]] ; then
+            echo "~/.$(basename ${file}) exists"
         else
-            echo "Linking ~/Public/dotfiles/${file} to ~/.${file}"
-            ln -s ~/Public/dotfiles/${file} ~/.${file}
+            echo "Linking ${file} to ~/.$(basename ${file})"
+            ln -s ${file} ~/.$(basename ${file})
         fi
     fi
 done
