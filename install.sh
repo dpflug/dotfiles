bd149a1ecc69c7c5c56e7b6a125975d92c9a7e79
David Pflug <david@tpflug.com>
2010-11-12 12:15:46 -0500

    install.sh: Adding support for vim directory. Some cleanup.

--- install.sh
+++ install.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
 
 #Ok, this is quick and hackish.
+
+REPODIR=$(pwd)
+
+if [[ ! -d ~/.config ]] ; then
+    mkdir ~/.config
+fi
+
 if [[ -a ~/.config ]] ; then
-    if [[ -a ~/.config/awesome ]] ; then
-        echo "~/.config/awesome exists"
-    else
+    if [[ ! -a ~/.config/awesome ]] ; then
         echo "Linking ~/.config/awesome"
-        ln -s ~/Public/dotfiles/config/awesome ~/.config/awesome
+        ln -s ${REPODIR}/config/awesome ~/.config/awesome
     fi
 fi
 
@@ -14,31 +19,39 @@ if [[ ! -d ~/.ssh ]] ; then
     mkdir ~/.ssh
 fi
 
-cd ~/Public/dotfiles/ssh
-for file in * ; do
+cd ${REPODIR}/ssh
+for file in $(find * -maxdepth 0 -type f) ; do
     if [[ -f ${file}.pub ]] ; then
         chmod 600 $file ${file}.pub
     fi
-    if [[ -a ~/.ssh/${file} ]] ; then
-        echo "~/.ssh/${file} exists"
-    else
-        echo "Linking ~/Public/dotfiles/ssh/${file} to ~/.ssh/"
-        ln -s ~/Public/dotfiles/ssh/${file} ~/.ssh/
+    if [[ ! -a ~/.ssh/${file} ]] ; then
+        echo "Linking ${REPODIR}/ssh/${file} to ~/.ssh/"
+        ln -s ${REPODIR}/ssh/${file} ~/.ssh/
     fi
 done
 
-for file in ~/Public/dotfiles/bin* ; do
-    ln -s $file ~/bin
-done
+if [[ ! -d ~/bin ]] ; then
+    ln -s $(pwd)/bin ~/bin
+else
+    for file in ${REPODIR}/bin/* ; do
+        if [[ ! -a ~/bin/$(basename ${file}) ]] ; then
+            ln -s $file ~/bin/
+        fi
+    done
+fi
+
+if [[ ! -d ~/.vim ]] ; then
+    ln -s $REPODIR/vim ~/.vim
+elif [[ ! -h ~/.vim ]] ; then
+    echo "~/.vim ALREADY EXISTS. Either delete it or merge them."
+fi
 
 
-for file in ~/Public/dotfiles/* ; do
+for file in ${REPODIR}/* ; do
     #Just omitting the directories for now. I'd like to pull them into this loop, too, though.
     #mkdir and ln files, or ln directories? How can I differentiate between dirs to link and dirs that will have local subdirs (ie .config)?
     if [[ -f $file && ! -x $file ]] ; then
-        if [[ -a ~/.$(basename ${file}) ]] ; then
-            echo "~/.$(basename ${file}) exists"
-        else
+        if [[ ! -a ~/.$(basename ${file}) ]] ; then
             echo "Linking ${file} to ~/.$(basename ${file})"
             ln -s ${file} ~/.$(basename ${file})
         fi
