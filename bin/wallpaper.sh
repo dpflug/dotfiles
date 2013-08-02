27e50159d724579f6101a7e171ae5bfb3e0f45b6
David Pflug <david@tpflug.com>
2013-08-01 22:21:09 -0400

    wallpaper.sh: Allow find to follow symlinks

--- bin/wallpaper.sh
+++ bin/wallpaper.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-DISPLAY="${DISPLAY:-:0}" feh --bg-fill "$(find ~/.config/awesome/theme/wallpapers -type f -iname '*.jpg' -o -iname '*.png' | shuf -n1)"
+DISPLAY="${DISPLAY:-:0}" feh --bg-fill "$(find -L ~/.config/awesome/theme/wallpapers/ -type f -iname '*.jpg' -o -iname '*.png' | shuf -n1)"
