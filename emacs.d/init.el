deb016a79523fa38952f19056fe89982aa5c119e
David Pflug <david@tpflug.com>
2013-09-12 10:21:16 -0400

    emacs: show-paren-mode

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(scheme-program-name "racket")
  '(send-mail-function (quote mailclient-send-it)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -46,6 +47,11 @@
 (add-hook 'lisp-interaction-mode-hook 'enable-paredit-mode)
 (add-hook 'scheme-mode-hook           'enable-paredit-mode)
 
+
+;And highlight parens, etc
+(require 'paren)
+(setq show-paren-style 'parenthesis)
+(show-paren-mode +1)
 ;Slime setup
 (load (expand-file-name "~/quicklisp/slime-helper.el"))
 (require 'slime)
