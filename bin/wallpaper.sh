2f944d890c8c5bc19d8879743955556f6a7dce81
David Pflug <david@tpflug.com>
2014-04-29 09:27:31 -0400

    wallpaper.sh: Determine DISPLAY

--- bin/wallpaper.sh
+++ bin/wallpaper.sh
@@ -1,2 +1,10 @@
 #!/usr/bin/env bash
-DISPLAY="${DISPLAY:-:0}" feh --bg-fill "$(find -L ~/.config/awesome/theme/wallpapers/ -type f -iname '*.jpg' -o -iname '*.png' | shuf -n1)"
+
+# Get the DISPLAY variable for the user's currently-running X, if it exists.
+read -rt 1 DISPLAY <(ps -ef | egrep 'user.*X' | grep -o ' :[[:digit:]]\+ ')
+
+if [[ -n "$DISPLAY" ]] ; then
+	feh --bg-fill "$(find -L ~/.config/awesome/theme/wallpapers/ -type f -iname '*.jpg' -o -iname '*.png' | shuf -n1)"
+else
+	return 0  # If X isn't running, we don't need to do anything.
+fi
