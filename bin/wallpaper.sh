d9a16aff01cfb798a49d8a424285e15c5f8e4489
David Pflug <david@tpflug.com>
2014-04-29 12:03:39 -0400

    wallpaper.sh: Better DISPLAY detection

--- bin/wallpaper.sh
+++ bin/wallpaper.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Get the DISPLAY variable for the user's currently-running X, if it exists.
-read -rt 1 DISPLAY <(ps -ef | egrep 'user.*X' | grep -o ' :[[:digit:]]\+ ')
+read -rt 1 DISPLAY < <(ps -ef | grep "$USER"'.*X' | grep -o ' :[[:digit:]]\+ ')
 
 if [[ -n "$DISPLAY" ]] ; then
 	feh --bg-fill "$(find -L ~/.config/awesome/theme/wallpapers/ -type f -iname '*.jpg' -o -iname '*.png' | shuf -n1)"
