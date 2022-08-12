16380256583a0c2fbeedb2c68d25178b364b1f48
David Pflug <david.pflug@exfo.com>
2022-08-12 14:40:47 -0400

    doom/config: change org dir

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -29,7 +29,7 @@
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
-(setq org-directory "/mnt/c/Users/ac46451/OneDrive - Lumen/Documents/EXFO/org")
+(setq org-directory "/mnt/c/pflug-enclave/EXFO/org")
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
